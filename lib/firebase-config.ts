3b9f8e790021408b0cd231ad48e9e4cc9a0ea3ed
Phil <walkerp07@yahoo.com>
2025-04-28 14:19:02 -0700

    Remove firebase config from tracking and update .gitignore

--- lib/firebase-config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCA4fViGoqNaZ1vNex0bj0vR20llB-Q0sI",
-  authDomain: "vibe-personal-finance.firebaseapp.com",
-  projectId: "vibe-personal-finance",
-  storageBucket: "vibe-personal-finance.firebasestorage.app",
-  messagingSenderId: "773209722059",
-  appId: "1:773209722059:web:aefd58000f9b1c127d1f4e",
-  measurementId: "G-EMC9GGW6CR",
-};
